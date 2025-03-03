908893d2e70c693ec55c5aa17e0213175727522f
UzzielAp <uzzielapariciok01@gmail.com>
2025-03-03 16:47:02 -0500

    Solucion de guard del login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,29 @@ import { ProductFormComponent } from './components/product-form/product-form.com
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth.guard';
+import { LoginRedirectGuard } from './guards/login-redirect.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'products', component: ProductListComponent, canActivate: [AuthGuard] },
-  { path: 'add', component: ProductFormComponent, canActivate: [AuthGuard] },
-  { path: 'edit/:id', component: ProductFormComponent, canActivate: [AuthGuard] },
+  { 
+    path: '', 
+    redirectTo: 'login', 
+    pathMatch: 'full' 
+  },
+  { path: 'login', 
+    component: LoginComponent, canActivate: [LoginRedirectGuard]
+  },
+  { path: 'home', 
+    component: HomeComponent, canActivate: [AuthGuard] 
+  },
+  { path: 'products', 
+    component: ProductListComponent, canActivate: [AuthGuard] 
+  },
+  { path: 'add', 
+    component: ProductFormComponent, canActivate: [AuthGuard] 
+  },
+  { path: 'edit/:id', 
+    component: ProductFormComponent, canActivate: [AuthGuard] 
+  },
   { path: '**', redirectTo: 'login' }
 ];
 
---
+++ src/app/guards/login-redirect.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginRedirectGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      const isAuthenticated = this.authService.isAuthenticated();
+
+      if (isAuthenticated) {
+        this.router.navigate(['/home']);
+        return false;
+      }
+      return true;
+  }
+}
